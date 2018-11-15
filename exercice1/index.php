438a66ecc3503d70edcb5534b22f7a973c35daf1
Luc <luc.lemahieu@laposte.net>
2018-11-15 16:19:21 +0100

    préparation des exos

---
+++ exercice1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <link href="style.css" />
+        <title>Exercice 1 PHP partie 3</title>
+    </head>
+    <body>
+        <?php
+            for($i= 0; $i != 11; $i++){
+                echo $i;
+                ?><br />
+        <?php
+            }
+        ?>
+    </body>
+</html>
---
+++ exercice6/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8" />
+        <link href="style.css" />
+        <title>Exercice 6 PHP partie 3</title>
+    </head>
+    <body>
+        <?php
+
+        ?>
+    </body>
+</html>
